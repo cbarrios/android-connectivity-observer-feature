22c08067dd9bd15c7207899a24f69508afaf0811
cbarrios <lalobarrios16@yahoo.com>
2022-10-26 14:32:52 -0400

    master: (4) NetworkConnectivityObserver implemented

---
+++ app/src/main/java/com/lalosapps/connectivityobserver/NetworkConnectivityObserver.kt
@@ -0,0 +1,46 @@
+package com.lalosapps.connectivityobserver
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+
+class NetworkConnectivityObserver(context: Context) : ConnectivityObserver {
+
+    private val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    override fun observeStatus(): Flow<Status> {
+        return callbackFlow {
+            val callback = object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    trySend(Status.AVAILABLE)
+                }
+
+                override fun onLosing(network: Network, maxMsToLive: Int) {
+                    super.onLosing(network, maxMsToLive)
+                    trySend(Status.LOSING)
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    trySend(Status.LOST)
+                }
+
+                override fun onUnavailable() {
+                    super.onUnavailable()
+                    trySend(Status.UNAVAILABLE)
+                }
+            }
+
+            connectivityManager.registerDefaultNetworkCallback(callback)
+            awaitClose {
+                connectivityManager.unregisterNetworkCallback(callback)
+            }
+        }.distinctUntilChanged()
+    }
+}
