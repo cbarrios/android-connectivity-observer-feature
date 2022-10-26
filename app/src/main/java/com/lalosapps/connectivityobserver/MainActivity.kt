f2605d15149d72ff163b54d207e21d994df99f61
cbarrios <lalobarrios16@yahoo.com>
2022-10-26 14:40:56 -0400

    master: (5) Lifecycle-aware flow collection

--- app/src/main/java/com/lalosapps/connectivityobserver/MainActivity.kt
+++ app/src/main/java/com/lalosapps/connectivityobserver/MainActivity.kt
@@ -2,10 +2,29 @@ package com.lalosapps.connectivityobserver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import com.lalosapps.connectivityobserver.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var connectivityObserver: ConnectivityObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
+
+        lifecycleScope.launch {
+            connectivityObserver.observeStatus()
+                .flowWithLifecycle(lifecycle)
+                .collect {
+                    binding.networkStatus.text = it.name
+                }
+        }
     }
 }
